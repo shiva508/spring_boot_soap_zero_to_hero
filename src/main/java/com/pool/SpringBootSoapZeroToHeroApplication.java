46c03764db7edaa784f8a21f59b98ba7bb2e9143
shiva508 <dasarishiva1@gmail.com>
2021-12-12 21:28:14 +0530

    first commit

---
+++ src/main/java/com/pool/SpringBootSoapZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapZeroToHeroApplication.class, args);
+	}
+
+}
